a628d99677399bcd9c9939c8cf5e544a93ae457b
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2023-02-21 10:18:04 +0300

    Simplified addition of urls for new tables

--- server.py
+++ server.py
@@ -3,30 +3,25 @@ import views
 from subprocess import call
 import db_updated_check
 #url rules created for each table
+
+def set_urls(app,table, view):
+    app.add_url_rule("/", view_func=views.home_page)
+    app.add_url_rule("/"+table+"/<int:page>", endpoint=table+"_data_page", view_func=view.get_table, methods=["GET","POST"])
+    app.add_url_rule("/"+table+"/add", endpoint=table+"_data_add", view_func=view.add_row,  methods=["POST"])
+    app.add_url_rule("/"+table+"/delete", endpoint=table+"_data_delete", view_func=view.delete_row, methods=["GET", "POST"])
+    app.add_url_rule("/"+table+"/update", endpoint=table+"_data_update", view_func=view.update_row, methods=["GET", "POST"])
+    app.add_url_rule("/"+table+"/search", endpoint=table+"_data_search", view_func=view.search, methods=["GET", "POST"])
+
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object("settings")
-    
-    fedView = views.Fed_Table()
-    app.add_url_rule("/", view_func=views.home_page)
-    app.add_url_rule("/fed/<int:page>", endpoint="fed_data_page", view_func=fedView.get_table, methods=["GET","POST"])
-    app.add_url_rule("/fed/add", endpoint="fed_data_add", view_func=fedView.add_row,  methods=["POST"])
-    app.add_url_rule("/fed/delete", endpoint="fed_data_delete", view_func=fedView.delete_row, methods=["GET", "POST"])
-    app.add_url_rule("/fed/update", endpoint="fed_data_update", view_func=fedView.update_row, methods=["GET", "POST"])
-    app.add_url_rule("/fed/search", endpoint="fed_data_search", view_func=fedView.search, methods=["GET", "POST"])
-   
-    priceView = views.Price_Table()
-    app.add_url_rule("/", view_func=views.home_page)
-    app.add_url_rule("/price/<int:page>", endpoint="price_data_page", view_func=priceView.get_table, methods=["GET","POST"])
-    app.add_url_rule("/price/add", endpoint="price_data_add", view_func=priceView.add_row,  methods=["POST"])
-    app.add_url_rule("/price/delete", endpoint="price_data_delete", view_func=priceView.delete_row, methods=["GET", "POST"])
-    app.add_url_rule("/price/update", endpoint="price_data_update", view_func=priceView.update_row, methods=["GET", "POST"])
-    app.add_url_rule("/price/search", endpoint="price_data_search", view_func=priceView.search, methods=["GET", "POST"])
+
+    set_urls(app, "fed", views.Fed_Table())
+    set_urls(app, "price", views.Price_Table())
    
 
     app.config["dbname"] = "import_test.db"
- 
-
     return app
 
 def create_db():
